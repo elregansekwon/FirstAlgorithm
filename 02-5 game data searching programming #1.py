9bfe1cc6c68b81fab600b245203835ce1d42fadd
elregansekwon <gwonseoyeon564@gmail.com>
2025-01-21 20:20:57 +0900

    첫 알고리즘 커밋 #4

--- 02-5 game data searching programming #1.py
+++ 02-5 game data searching programming #1.py
@@ -7,4 +7,8 @@ print(x.index('다람쥐'))
 x=[['raccoon',10,300],['fox',40,60],['reindeer',20,500],['cat',70,800]]
 print((x))
 
+# 사용자 이름이 'reindeer'인 게임 아이템 정보 꺼내기
 print([y for y in x if y[0]=='reindeer'])
+# for y in x: 바깥 배열 x에 있는 안쪽 배열 y 차례대로 검사
+# if y[0]=='reindeer': 안쪽 배열의 1번째 데이터가 reindeer이면
+# [y ~~~]: y 배열을 []안에 넣어서 출력
